173df41fe5a193bfe25a870309705098bc64f7f2
Nick Rains <nrains1020@gmail.com>
2024-03-05 12:01:40 -0500

    Add hash table and address_data

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from package import Package
+from chaining_hash import ChainingHashTable
 
 package_data = Package.read_package_data("package_data.csv")
 
 loaded_packages = Package.load_package(package_data)
 
+myHash = ChainingHashTable()
+
 for package in loaded_packages:
-    print(package)
+    myHash.insert(package.package_id, package)
 
-
+for i in range(1, 41):
+    print(myHash.search(str(i)))
 
 
     
